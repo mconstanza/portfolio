8f8c242ed0cf163ede0c08946cc7e255b40e2b89
Mike Constanza <michael.constanza@gmail.com>
2017-01-31 20:55:33 -0500

    set up github links to open in new tab

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,8 +7,8 @@ class Project extends Component {
         <div className="project">
           <img className="projectImg" src={this.props.image}/>
           <div className="projectOverlay">
-            <a href={this.props.url}><img className="projectDeployIcon" src='./img/monitor.png'/></a>
-            <a href={this.props.git}><img className="projectGitIcon" src='./img/github-logo.png'/></a>
+            <a target="blank" href={this.props.url}><img className="projectDeployIcon" src='./img/monitor.png'/></a>
+            <a target="blank" href={this.props.git}><img className="projectGitIcon" src='./img/github-logo.png'/></a>
           </div>
         </div>
 
