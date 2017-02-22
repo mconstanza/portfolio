7145b9d2a23e85ce01df2e8048d73ffa112f395b
Mike Constanza <michael.constanza@gmail.com>
2017-02-22 13:18:40 -0500

    added mvc skill

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -24,6 +24,7 @@ class Skills extends Component {
                 <Skill skill="Express.js" icon="./img/skills/node.png" link="https://expressjs.com/en/api.html"/>
                 <Skill skill="MySQL" icon="./img/skills/mysql.png" link="https://dev.mysql.com/doc/"/>
                 <Skill skill="MongoDB" icon="./img/skills/mongoLeaf.png" link="https://docs.mongodb.com/manual/"/>
+                <Skill skill="MVC Architecture" icon="./img/skills/curly_braces.png" link="https://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller"/>
                 <Skill skill="RESTful Services" icon="./img/skills/curly_braces.png" link="https://en.wikipedia.org/wiki/Representational_state_transfer"/>
                 <Skill skill="React.js" icon="./img/skills/react.png" link="https://facebook.github.io/react/"/>
             </ul>
